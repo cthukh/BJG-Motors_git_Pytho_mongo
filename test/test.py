3aef52729aceef564271bf22b220a45586a4346c
jairo vargas <165170124+cthukh@users.noreply.github.com>
2026-07-07 16:16:46 -0400

    Delete test directory

--- test/test.py
+++
@@ -1,8 +0,0 @@
-print(
-    "Menu principal: \n"
-    "\ 1) Ver opciones de Funcionarios \n"
-    "\ 2) Ver opciones de Reparaciones \n"
-    "\ 4) Ver opciones de Vehiculos \n\n"
-
-    "\ 0) salir del sistema \n"
-)
